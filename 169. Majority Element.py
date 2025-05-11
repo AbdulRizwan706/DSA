479f8080d08ae540a0b9272ffd3409b024141688
AbdulRizwan706 <abdulrizwan706@gmail.com>
2025-05-12 00:48:57 +0530

    Added code

---
+++ 169. Majority Element.py
@@ -0,0 +1,11 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        index = 0
+        checker = {}
+        for i in range(len(nums)):
+            if checker.get(nums[i], 0) < 2:
+                nums[index] = nums[i]
+                checker[nums[i]] = checker.get(nums[i], 0) + 1
+                index += 1
+
+        return index
